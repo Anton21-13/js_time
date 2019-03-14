20d4b209b16f82a8255b1070bff8c33c986767be
Антон Мещеряков <anton23.pochta@gmail.com>
2019-03-15 00:16:45 +0500

    Make present time

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 'use strit';
 
-let 
+function times() {
+  let time = new Date(),
+    hour = time.getHours(),
+    minute = time.getMinutes(),
+    second = time.getSeconds();
+
+  if (hour < 10) hour = "0" + hour;
+  if (minute < 10) minute = "0" + minute;
+  if (second < 10) second = "0" + second;
+
+  document.querySelector('#time').textContent = hour + ":" + minute + ":" + second;
+    
+}
+
+setInterval(times, 1000);
